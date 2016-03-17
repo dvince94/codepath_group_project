54715ca18d6e8b818479dcd384dbf2edeba0d39c
Kevin Duong <kdy480@yahoo.com>
2016-03-16 20:14:24 -0400

    login/logout, logout button is only on first tab view controller right now

---
+++ Recipes/RecipeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RecipeViewController.swift
+//  Recipes
+//
+//  Created by Kevin Duong on 3/16/16.
+//  Copyright © 2016 CodepathGroupProject. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    static let userDidLogoutNotification = "userDidLogout"
+    
+    @IBAction func onLogout(sender: AnyObject) {
+        PFUser.logOutInBackgroundWithBlock { (error: NSError?) -> Void in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                print("logout true")
+                NSNotificationCenter.defaultCenter().postNotificationName(RecipeViewController.userDidLogoutNotification, object: nil)
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
