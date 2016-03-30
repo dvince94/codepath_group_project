72b34164705d06912e05e1bb5d8baaa47361f9b0
Vincent Duong <vduong94@gmail.com>
2016-03-30 11:24:33 -0400

    Added challenge

--- Recipes/AddChallengeViewController.swift
+++ Recipes/AddChallengeViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class AddChallengeViewController: UIViewController {
-
+class AddChallengeViewController: UIViewController, UITextViewDelegate  {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var ingredientsTextView: UITextView!
+    @IBOutlet weak var descriptionTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ingredientsTextView.text = "Add ingredients and separate each ingredients by a comma. For example, Tomatoes, Lettuce, Cucumber,..."
+        ingredientsTextView.textColor = UIColor.lightGrayColor()
+        
+        descriptionTextView.text = "Add description here."
+        descriptionTextView.textColor = UIColor.lightGrayColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +26,89 @@ class AddChallengeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Mark Save and Cancel
+    
+    @IBAction func cancelButtonTapped(sender: AnyObject){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(sender: AnyObject) {
+        if (containText(titleTextField.text!) == false) {
+            showAlertMessage("No Title Added", messages: "Please add a title.")
+        }
+        else if (containText(ingredientsTextView.text!) == false || ingredientsTextView.textColor == UIColor.lightGrayColor()) {
+            showAlertMessage("No Ingredients Added", messages: "Please add an ingredient.")
+        }
+        else if (containText(descriptionTextView.text!) == false || descriptionTextView.textColor == UIColor.lightGrayColor()) {
+            showAlertMessage("No Descripions Added", messages: "Please add a description.")
+        }
+        else {
+            let ingredients = parseIngredients(ingredientsTextView.text!)
+            //Post challenge
+            Challenge.postChallenge(title: titleTextField.text!, withIngredients: ingredients, withDescription: descriptionTextView.text!, withCompletion: { (success, error) -> Void in
+                if success == true {
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                }
+                else {
+                    print("\(error?.localizedDescription)")
+                }
+            })
+        }
+    }
+    
+    //Check if it contains any text
+    func containText(str: String) -> Bool {
+        let whiteSpaceSet = NSCharacterSet.whitespaceCharacterSet()
+        if str.stringByTrimmingCharactersInSet(whiteSpaceSet) != "" {
+            return true
+        }
+        return false
+    }
+    
+    //Parse ingredients
+    func parseIngredients(str: String) -> [String] {
+        let ingredients =  str.componentsSeparatedByString(", ")
+        //filter out empty strings
+        return ingredients.filter(){$0 != ""}
+    }
+    
+    //Show error message
+    func showAlertMessage(titles: String, messages: String) {
+        let alertController = UIAlertController(title: "\(titles)", message: "\(messages)", preferredStyle: .Alert)
+        
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        
+        presentViewController(alertController, animated: true) {
+            // optional code for what happens after the alert controller has finished presenting
+        }
+    }
+    
+    //MARK: - TextView Handling Method
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        //Get rid of placeholder
+        if textView.textColor == UIColor.lightGrayColor() {
+            textView.text = nil
+            textView.textColor = UIColor.blackColor()
+        }
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        //Replace placeholder if text is empty
+        if containText(ingredientsTextView.text!) == false {
+            textView.text = "Add ingredients and separate each ingredients by a comma. For example, Tomatoes, Lettuce, Cucumber,..."
+            textView.textColor = UIColor.lightGrayColor()
+        }
+        if containText(descriptionTextView.text!) == false {
+            textView.text = "Add description here."
+            textView.textColor = UIColor.lightGrayColor()
+        }
+    }
 
     /*
     // MARK: - Navigation
